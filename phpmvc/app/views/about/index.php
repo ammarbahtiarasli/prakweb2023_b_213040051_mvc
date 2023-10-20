ae467f8ec43c9955a3421f08bf5dc879fdacabc3
ammarbahtiarasli <ammarbahtiarasli@gmail.com>
2023-10-20 21:45:14 +0700

    phpmvc #6

--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="https://lh3.googleusercontent.com/pw/AM-JKLVVYNADTrjeomPl9IuZfh-b-PpHNyNrYtbck8y5VfX9gyq_jvo1TsvUErth44SI5gVl1JcQXKWA88dwmXQ8VRi4W6vuRltpiSOzYZyNg0Vpj58j4ZvHwZC1PV9f7_OlRjRDL7X5Pm99umA2REgzA3k=s943-no?authuser=0" alt="Ammar" width="200" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
