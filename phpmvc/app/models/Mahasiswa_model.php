49a5e68331dd30c6bef0421df69a1ec4be8ae098
ammarbahtiarasli <ammarbahtiarasli@gmail.com>
2023-10-21 19:24:44 +0700

    phpmvc #8 dan #9, lupa di push pas ngerjain video ke-8 mohon maaf, terima kasih.

--- phpmvc/app/models/Mahasiswa_model.php
+++ phpmvc/app/models/Mahasiswa_model.php
@@ -1,25 +1,40 @@
 <?php
 
 class Mahasiswa_model {
-    private $stmt;
-    private $dbh;
+
+    private $table = 'mahasiswa';
+    private $db;
 
     public function __construct()
     {
-        // data source name
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-        try {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch(PDOException $e) {
-            die($e->getMessage());
-        }
+        $this->db = new Database;
     }
 
     public function getAllMahasiswa()
     {
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
+    }
+
+    public function getMahasiswaById($id)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
+
+    public function tambahDataMahasiswa($data)
+    {
+        $query = "INSERT INTO mahasiswa(nama , nrp, email, jurusan) VALUES
+                  (:nama, :nrp, :email, :jurusan)";
+
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+
+        $this->db->execute();
+        return $this->db->rowCount();
     }
 }
--- phpmvc/app/views/mahasiswa/index.php
+++ phpmvc/app/views/mahasiswa/index.php
@@ -1,15 +1,60 @@
-<div class="container mt-4">
+<div class="container mt-3">
     <div class="row">
         <div class="col-6">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+                Tambah data mahasiswa
+            </button>
+            <br><br>
+
+            <!-- Modal -->
+            <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="judulModal">Tambah data mahasiswa</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                                <div class="mb-3">
+                                    <label for="nama" class="form-label">Nama</label>
+                                    <input type="text" class="form-control" id="nama" name="nama">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="nrp" class="form-label">Nrp</label>
+                                    <input type="number" class="form-control" id="nrp" name="nrp">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input type="email" class="form-control" id="email" name="email">
+                                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="nrp" class="form-label">Jurusan</label>
+                                    <select class="form-select" aria-label="jurusan" name="jurusan" id="jurusan">
+                                        <option value="Teknik Informatika">Teknik Informatika</option>
+                                        <option value="Teknik Mesin">Teknik Mesin</option>
+                                        <option value="Teknik Industri">Teknik Industri</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <h3>Daftar Mahasiswa</h3>
-                <?php foreach( $data['mhs'] as $mhs ) : ?>
-                    <ul>
-                        <li><?= $mhs['nama']; ?></li>
-                        <li><?= $mhs['nrp']; ?></li>
-                        <li><?= $mhs['email']; ?></li>
-                        <li><?= $mhs['jurusan']; ?></li>
-                    </ul>
+            <ul class="list-group">
+                <?php foreach ($data['mhs'] as $mhs) : ?>
+                    <li class="list-group-item d-flex justify-content-between align-items-center"><?= $mhs['nama']; ?>
+                        <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge bg-primary">Detail</a>
+                    </li>
                 <?php endforeach; ?>
+            </ul>
         </div>
     </div>
 </div>
