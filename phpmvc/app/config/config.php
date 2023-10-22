563d782bc75517c0a67f1abcd242d499284fa059
ammarbahtiarasli <ammarbahtiarasli@gmail.com>
2023-10-22 21:57:08 +0700

    phpmvc #12 dan #13 (tamat), pas ngerjain video 12 lupa push :)

--- phpmvc/app/config/config.php
+++ phpmvc/app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASEURL', 'http://mvc.euy/phpmvc/public');
+define('BASEURL', 'http://localhost/mvc/phpmvc/public');
 
 // DB
 define('DB_HOST', 'localhost');
--- phpmvc/app/views/mahasiswa/index.php
+++ phpmvc/app/views/mahasiswa/index.php
@@ -4,25 +4,58 @@
         <div class="col-lg-6">
             <?php Flasher::flash(); ?>
         </div>
+    </div>
+
+    <div class="row mb-3">
+        <div class="col-lg-6">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal" data-bs-target="#formModal">
+                Tambah data mahasiswa
+            </button>
+        </div>
+    </div>
+
+    <div class="row mb-3">
+        <div class="col-lg-6">
+            <form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Cari mahasiswa ..." id="keyword" name="keyword" autocomplete="off">
+                <button class="btn btn-primary" type="submit" id="tombolCari">Cari</button>
+            </div>
+            </form>
+        </div>
+    </div>
 
     <div class="row">
         <div class="col-lg-6">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
-                Tambah data mahasiswa
-            </button>
-            <br><br>
+
+            <h3>Daftar Mahasiswa</h3>
+            <ul class="list-group">
+                <?php foreach ($data['mhs'] as $mhs) : ?>
+                    <li class="list-group-item"><?= $mhs['nama']; ?>
+                        <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge bg-primary float-end ms-1">Detail</a>
+
+                        <!-- ubah -->
+                        <a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge bg-success float-end ms-1 tampilModalUbah" data-bs-toggle="modal" data-bs-target="#formModal" data-id="<?= $mhs['id']; ?>">Ubah</a>
+
+                        <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge bg-danger
+                        float-end ms-1" onclick="return confirm('yakin?');">Hapus</a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
 
             <!-- Modal -->
-            <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+            <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="formModal" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="judulModal">Tambah data mahasiswa</h1>
+                            <!-- Title -->
+                            <h1 class="modal-title fs-5" id="formModalLabel"></h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                             <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                                <input type="hidden" name="id" id="id">
                                 <div class="mb-3">
                                     <label for="nama" class="form-label">Nama</label>
                                     <input type="text" class="form-control" id="nama" name="nama">
@@ -44,25 +77,15 @@
                                         <option value="Teknik Industri">Teknik Industri</option>
                                     </select>
                                 </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Tambah Data</button>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Tambah Data</button>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
-            <h3>Daftar Mahasiswa</h3>
-            <ul class="list-group">
-                <?php foreach ($data['mhs'] as $mhs) : ?>
-                    <li class="list-group-item"><?= $mhs['nama']; ?>
-                        <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge bg-primary float-end ms-1">Detail</a>
-                        <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge bg-danger
-                        float-end ms-1" onclick="return confirm('yakin?');">Hapus</a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
         </div>
     </div>
 </div>
